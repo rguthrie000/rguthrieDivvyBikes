7968976d94b6638d8e892952504b1933c9bf0bf4
rguthrie <rguthrie000@gmail.com>
2020-02-26 23:49:39 -0500

    crashing on heroku. edited server.js to not use .env variable for session initialization

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === "production") {
 // Session specification - session is established on valid login
 app.use(
   session({
-    secret: process.env.secret,
+    secret: 'profoundkeyphrase',
     resave: true,
     saveUninitialized: true
   })
