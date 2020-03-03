2fb2e07db64f32d55907b6f603e6a8d3739ea86a
rguthrie <rguthrie000@gmail.com>
2020-03-03 14:16:22 -0500

    Moved bikerides_db to MongoDB Atlas; added logging and (on heroku) edited heroku environment variable.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Float = require('mongoose-float').loadType(mongoose,8);
- 
+const MongoClient = require('mongodb').MongoClient;
+
 //*******************************
 //***   Database Connection   ***
 //*******************************
@@ -10,11 +11,20 @@ const dbName = "bikerides_db";
 
 mongoose.connect(
   process.env.MONGODB_URI ? process.env.MONGODB_URI : ("mongodb://localhost/"+dbName), 
-  {useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true}
+  {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
 );
+
+if (debug) {
+  if (process.env.MONGODB.URI) {
+    console.log(`heroku deployment; MongoDB Atlas at ${process.env.MONGODB_URI}`);
+  } else {
+    console.log('using local MongoDB');
+  }
+}
 
 // Get the connection
 const db = mongoose.connection;
+
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
